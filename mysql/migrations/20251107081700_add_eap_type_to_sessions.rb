bea33d2ddc7ea55ee0c45be78217562e0e171a22
Oisin Mulvihill <oisin.mulvihill@gmail.com>
2025-11-12 13:47:18 +0000

    add reason for 254 which is max permitted email length including the @ character

--- mysql/migrations/20251107081700_add_eap_type_to_sessions.rb
+++ mysql/migrations/20251107081700_add_eap_type_to_sessions.rb
@@ -3,7 +3,8 @@ Sequel.migration do
     alter_table :sessions do
       add_column :eap_type, String, default: ""
       # Increase username size to accommodate longer usernames such as email
-      # addresses which come in via some EAP types.
+      # addresses which come in via some EAP types. 254 is max length for an
+      # email address as per RFC 5321.
       set_column_type :username, String, size: 254
     end
   end
